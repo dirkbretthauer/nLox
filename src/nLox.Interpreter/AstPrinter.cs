eb425211b8d8220a01a4bb89d6df78dd212f2864
Dirk Bretthauer <dirk.bretthauer@de.bosch.com>
2022-02-17 15:04:14 +0100

    til chapter 6

---
+++ src/nLox.Interpreter/AstPrinter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nLox.Interpreter
+{
+    internal class AstPrinter : Expression.IVisitor<string>
+    {
+        static void Main(string[] args)
+        {
+            Expression expression = new Expression.Binary(
+                new Expression.Unary(
+                    new Token(TokenType.MINUS, "-", null, 1),
+                    new Expression.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expression.Grouping(
+                    new Expression.Literal(45.67)));
+
+            Console.WriteLine(new AstPrinter().Print(expression));
+        }
+
+        public string Print(Expression expression)
+        {
+            return expression.Accept(this);
+        }
+
+        public string VisitBinaryExpression(Expression.Binary expression)
+        {
+            return Parenthesize(expression.OperatorToken.Lexeme, expression.Left, expression.Right);
+        }
+
+        public string VisitGroupingExpression(Expression.Grouping expression)
+        {
+            return Parenthesize("group", expression.Expression);
+        }
+
+        public string VisitLiteralExpression(Expression.Literal expression)
+        {
+            if (expression.Value == null)
+                return "nil";
+
+            return expression.Value.ToString()!;
+        }
+
+        public string VisitUnaryExpression(Expression.Unary expression)
+        {
+            return Parenthesize(expression.OperatorToken.Lexeme, expression.Right);
+        }
+
+        private string Parenthesize(string name, params Expression[] expressions)
+        {
+            var builder = new StringBuilder();
+            
+            builder.Append("(").Append(name);
+            foreach(var expression in expressions)
+            {
+                builder.Append(" ");
+                builder.Append(expression.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/nLox.Interpreter/Expression.cs
@@ -0,0 +1,78 @@
+namespace nLox.Interpreter
+
+{
+  public abstract class Expression
+  {
+    public interface IVisitor<R>
+    {
+      R VisitBinaryExpression(Binary expression);
+      R VisitGroupingExpression(Grouping expression);
+      R VisitLiteralExpression(Literal expression);
+      R VisitUnaryExpression(Unary expression);
+    }
+    public class Binary : Expression
+    {
+      public Expression Left { get; }
+      public Token OperatorToken { get; }
+      public Expression Right { get; }
+
+      public Binary(Expression Left, Token OperatorToken, Expression Right)
+      {
+        this.Left = Left;
+        this.OperatorToken = OperatorToken;
+        this.Right = Right;
+      }
+
+      public override R Accept<R>(IVisitor<R> visitor)
+      {
+        return visitor.VisitBinaryExpression(this);
+      }
+    }
+    public class Grouping : Expression
+    {
+      public Expression Expression { get; }
+
+      public Grouping(Expression Expression)
+      {
+        this.Expression = Expression;
+      }
+
+      public override R Accept<R>(IVisitor<R> visitor)
+      {
+        return visitor.VisitGroupingExpression(this);
+      }
+    }
+    public class Literal : Expression
+    {
+      public Object Value { get; }
+
+      public Literal(Object Value)
+      {
+        this.Value = Value;
+      }
+
+      public override R Accept<R>(IVisitor<R> visitor)
+      {
+        return visitor.VisitLiteralExpression(this);
+      }
+    }
+    public class Unary : Expression
+    {
+      public Token OperatorToken { get; }
+      public Expression Right { get; }
+
+      public Unary(Token OperatorToken, Expression Right)
+      {
+        this.OperatorToken = OperatorToken;
+        this.Right = Right;
+      }
+
+      public override R Accept<R>(IVisitor<R> visitor)
+      {
+        return visitor.VisitUnaryExpression(this);
+      }
+    }
+
+    public abstract R Accept<R>(IVisitor<R> visitor);
+  }
+}
